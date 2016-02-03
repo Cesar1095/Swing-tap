bd82d0d4df9c99ccd4deabc173b99324448556c9
HectorGaray <hemagarayoc@ittepic.edu.mx>
2016-02-03 11:47:09 -0600

    JToolBar,JTextfiel,JTextArea

--- GUI.java
+++ GUI.java
@@ -3,7 +3,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.JPanel
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+import javax.swing.JTextArea;
+
 public class GUI
 {
   public static void main(String[] args)
@@ -19,5 +23,29 @@ public class GUI
     JFrame frame = new JFrame("JOptionPane showMessageDialog component example");
     JOptionPane.showMessageDialog(frame, panel);
     System.exit(0);
-  }
+    
+ JFrame ventana= new JFrame("COmponentes");
+frame.setBounds(10,10,600,600);
+frame.setVisible(true);
+frame.setTitle("Componentes de una interfaz");
+frame.setResizable(false); 
+
+
+JTextField caja= new JTextField("JTextField");
+caja.setBounds(60,15,150,20);
+frame.add(caja);
+
+JTextArea Area= new JTextArea();
+Area.setBounds(100,110,23,300);
+Area.setVisible(true);
+frame.add(Area);
+
+
+JToolBar barra = new JToolBar("barra");
+ barra.add(caja);
+ frame.getContentPane().add(barra); 
+ barra.setVisible(true);
+
+}
+  
 }
